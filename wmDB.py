b623aadefc02db240d0831110d572c02e1611d92
hdg-utd <hdg150130@utdallas.edu>
2017-01-05 11:49:01 +0530

    Added extra finctionality

--- wmDB.py
+++ wmDB.py
@@ -1,13 +1,20 @@
 import urllib2
 from bs4 import BeautifulSoup
 
-url_string = "http://www.watermark.org/message/44000"
+url_string = "http://www.watermark.org/message/4400"
 
 try:
 	response_code = urllib2.urlopen(url_string).getcode()
 	if(response_code != 200):
 		print "Not 200"
 	else:
-		print response_code
+		web_page = urllib2.urlopen(url_string).read()
+		soup = BeautifulSoup(web_page, "lxml")
+
+		title_parent = soup.findAll('h2', {'class': 'message-title'})[0]
+		for title_child in title_parent.children:
+			title_string = title_child.contents[0]
+		if(title_string != ""):
+			date_created = soup.findAll('span', {'class': 'date'})[0].contents[0]
 except:
 	print "Not connected"
